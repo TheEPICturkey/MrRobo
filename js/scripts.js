69eb11c028a8225e41b6caf895504baa771356b7
Brandon Spear <Bowdowntotheepicturkey@gmail.com>
2023-02-09 14:41:24 -0800

    added UI and BL code.

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+// Business Logic
+
+function beepBoop() {
+  const numb1Input = document.getElementById("numberInput").value;
+  let outputArray = [];
+  for (let i = 0; i <= numb1Input; i++) {
+    let str = i.toString()
+    if (str.includes("3")) {
+      outputArray.push("Won't you be my neighbor?")
+    } else if (str.includes("2")) {
+      outputArray.push("boop")
+    } else if (str.includes("1")) {
+      outputArray.push("beep")
+    } else {
+      outputArray.push(i);
+    }
+    };
+    const finalOutput = outputArray.join(", ");
+    return finalOutput
+  }
+  
+  // UI Logic
+  
+  function formHandler(event) {
+    event.preventDefault();
+    const numb1Input = document.getElementById("numberInput").value;
+    document.getElementById("result").innerText = "";
+    if (numb1Input === "") {
+      document.getElementById("result").innerText = "We can't count nothing! Go ahead and put a number in!"
+    } else {
+      document.getElementById("result").innerText = "Results: " + beepBoop();
+    }
+  }
+  
+  window.addEventListener("load", function() {
+    document.getElementById("countUp").addEventListener("submit", formHandler);
+    console.log("page load function worked")
+  });
